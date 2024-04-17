f4fd427c382b8cdbd38ba196037f8aa4102123a2
RamiroGuerra <ramiro.guerra03@utrgv.edu>
2024-04-17 11:52:10 -0500

    testing

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php 
+    echo 'hello ' .htmlspecialchars($_GET["name"]) . '!';
+    // echo 'welcome to php <br/>';
+    // echo date('Y-m-d H:i:s');
+    // echo'one way to acquire php on vscode is downloading php compiler'; 
+    // echo'then two extensions that are PHP Intelephense and php server';
+    // echo'as required from the php page: php itself, a webserver and browser'; 
+        
+    ?>
+    
+</body>
+</html>
